6d7d7b90cfa5a9299a502f9fb997211bd1e2b28e
lUCI <lusindisontanjana@gmail.com>
2022-06-29 18:32:15 +0200

    render rockets

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -2,21 +2,21 @@ import PropTypes from 'prop-types';
 import classes from './Rocket.module.css';
 
 const Rocket = ({ src, name, description }) => (
-	<section className={classes.rocket}>
-		<div>
-			<img src={src} alt={name} />
-		</div>
-		<div>
-			<h2>{name}</h2>
-			<p>{description}</p>
-			<button type="button">Reserve rocket</button>
-		</div>
-	</section>
+  <section className={classes.rocket}>
+    <div>
+      <img src={src} alt={name} />
+    </div>
+    <div>
+      <h2>{name}</h2>
+      <p>{description}</p>
+      <button type="button">Reserve rocket</button>
+    </div>
+  </section>
 );
 export default Rocket;
 
 Rocket.propTypes = {
-	name: PropTypes.string.isRequired,
-	src: PropTypes.string.isRequired,
-	description: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
 };
