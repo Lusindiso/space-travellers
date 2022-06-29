c42739bf882baaa4dc660beb5e29458c1f226ceb
principles31 <principles31@gmail.com>
2022-06-29 14:39:04 +0000

    Add basic style to the mission display

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-
+import Missions from './components/Missions';
+import Profiles from './components/Profiles';
+import Rockets from './components/Rockets';
 import './App.css';
-import Navbar from './components/Navbar';
-import Missions from './components/pages/Missions';
-import MyProfile from './components/pages/MyProfile';
-import Rockets from './components/pages/Rockets';
 
 function App() {
   return (
-    <>
-      <Navbar />
+    <div className="App">
       <Routes>
         <Route path="/" element={<Rockets />} />
         <Route path="/missions" element={<Missions />} />
-        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/my-profile" element={<Profiles />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
