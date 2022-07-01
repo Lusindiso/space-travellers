e93caf81e30017e13139e1c60b2ef022ee57c0db
Lusindiso Ntanjana <ntanjanalusindiso8@gmail.com>
2022-07-01 09:55:53 +0200

    Update Rockets.test.js

--- src/components/__TEST__/Rockets.test.js
+++ src/components/__TEST__/Rockets.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import store from '../../redux/index';
+import store from '../../redux/configureStore';
 import Rockets from '../pages/Rockets';
 
 describe('Testing rocket component', () => {});
