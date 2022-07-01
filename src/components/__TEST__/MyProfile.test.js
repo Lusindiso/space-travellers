b397e760634721b87aca44e838bb83c2eb76e248
Lusindiso Ntanjana <ntanjanalusindiso8@gmail.com>
2022-07-01 09:54:52 +0200

    Update MyProfile.test.js

--- src/components/__TEST__/MyProfile.test.js
+++ src/components/__TEST__/MyProfile.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import store from '../../redux/index';
+import store from '../../redux/configureStore';
 import Profile from '../pages/MyProfile';
 
 describe('Testing mission component', () => {
